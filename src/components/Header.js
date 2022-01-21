1991a8551638231b7598525015eaa395b2907e68
Shittu Ibrahim <shit2wice@gmail.com>
2022-01-21 21:52:34 +0000

    [feat] Add Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,63 @@
 import React from "react";
+import styled from "styled-components";
+import Avatar from "@mui/material/Avatar";
+
+import logo from "../images/logo.svg";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCartOutlined";
+import profilePicture from "../images/image-avatar.png";
 
 const Header = () => {
-  return <div>This is the Header</div>;
+  return (
+    <HeaderContainer>
+      <HeaderLeft>
+        <Image>
+          <img src={logo} alt="logo" />
+        </Image>
+        <p>Collections</p>
+        <p>Men</p>
+        <p>Women</p>
+        <p>About</p>
+        <p>Contact</p>
+      </HeaderLeft>
+      <HeaderRight>
+        <ShoppingCartIcon />
+        <Avatar src={profilePicture} alt="profile" />
+      </HeaderRight>
+      <HeaderRight></HeaderRight>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
+
+const HeaderContainer = styled.div`
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: 400;
+  border-bottom: 1px solid hsl(223, 64%, 98%);
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-right: auto;
+
+  > p {
+    margin-left: 20px;
+    color: hsl(219, 9%, 45%);
+  }
+`;
+
+const Image = styled.div`
+  margin-right: 20px;
+`;
+
+const HeaderRight = styled.div`
+  display: flex;
+  width: 100px;
+  justify-content: space-around;
+  align-items: center;
+`;
